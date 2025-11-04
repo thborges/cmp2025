09b6cb7beb8a0b41b079623b9e1fc24f8733515a
Thiago Borges de Oliveira <thborges@gmail.com>
2025-11-04 13:46:52 -0300

    Aula do dia 28/10

---
+++ nodes.h
@@ -0,0 +1,26 @@
+
+#include<vector>
+
+extern int yylineno;
+
+using namespace std;
+
+class Node {
+protected:
+    vector<Node*> children;
+    int lineno;
+
+public:
+    Node() {
+        lineno = yylineno;
+    }
+
+    int getLineNo() {
+        return lineno;
+    }
+
+    void append(Node *n) {
+        children.push_back(n);
+    }
+};
+
